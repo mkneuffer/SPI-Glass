f503f6fe7b58e754814f00bd077423c3801511dc
Sumi Hayasakura <145051511+soomballs@users.noreply.github.com>
2024-10-21 16:48:07 -0400

    Inventory Manager, Item Data
    Inventory Manager and Item Data Implemented, Inventory Scene created but not edited as of now

--- SPI-Glass/Assets/Scripts/ItemData.cs
+++ SPI-Glass/Assets/Scripts/ItemData.cs
@@ -7,17 +7,9 @@ public class ItemData : MonoBehaviour
 
     // What does item need
     public bool inInventory;
+    public string name;
+    public int itemNum;
     [SerializeField] private GameObject itemModel;
     [SerializeField] private GameObject itemIcon;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
