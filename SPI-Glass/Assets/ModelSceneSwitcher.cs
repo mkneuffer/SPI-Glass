9fdcfbcaa1992601101986233ad6826b20be6427
mkneuffer <126025888+mkneuffer@users.noreply.github.com>
2024-11-18 12:17:44 -0500

    Map to Scene 2

--- SPI-Glass/Assets/ModelSceneSwitcher.cs
+++ SPI-Glass/Assets/ModelSceneSwitcher.cs
@@ -3,19 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcherOnClick : MonoBehaviour
 {
-    [Tooltip("Name of the scene to switch to when the object is clicked")]
-    public string SceneName;
-
     void OnMouseDown()
     {
-        // Check if the SceneName is not empty
-        if (!string.IsNullOrEmpty(SceneName))
-        {
-            SceneManager.LoadScene(SceneName);
-        }
-        else
-        {
-            Debug.LogWarning("SceneName is not set. Please set it in the Inspector.");
-        }
+        Debug.Log("Object clicked!");
+        SceneManager.LoadScene(2);
     }
 }
