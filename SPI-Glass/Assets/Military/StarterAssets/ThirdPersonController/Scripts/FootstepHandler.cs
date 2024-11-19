57bb845f550b04a85a2f7340cc8a9c71f3210349
mkneuffer <126025888+mkneuffer@users.noreply.github.com>
2024-11-19 18:28:21 -0500

    Updating Player

---
+++ SPI-Glass/Assets/Military/StarterAssets/ThirdPersonController/Scripts/FootstepHandler.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FootstepHandler : MonoBehaviour
+{
+    public void OnFootstep()
+    {
+        // Add code to handle the footstep event, e.g., play a sound
+        Debug.Log("Footstep event triggered");
+    }
+}
