60c040db15cefb332328994f2aaafef6d235ddf2
Sumi Hayasakura <145051511+soomballs@users.noreply.github.com>
2024-12-05 19:20:17 -0500

    lots of ui tings

---
+++ SPI-Glass/Assets/Scripts/MapUIManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapUIManager : MonoBehaviour
+{ 
+
+    [SerializeField] Image settingsButton;
+    [SerializeField] GameObject expandMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        expandMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openMenu()
+    {
+        expandMenu.SetActive(true);
+    }
+
+    public void closeMenu()    
+    {
+        expandMenu.SetActive(false);
+    }
+
+    public void toggleMenu()
+    {
+        if (expandMenu.activeSelf == true)
+        {
+            expandMenu.SetActive(false);
+        }
+        else
+        {
+            expandMenu.SetActive(true);
+        }
+    }
+}
