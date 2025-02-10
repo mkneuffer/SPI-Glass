47cefae1cb74e5793eebebfeb5a74db656d652cd
mkneuffer <126025888+mkneuffer@users.noreply.github.com>
2025-02-10 13:37:19 -0500

    Lasers
    Lasers

--- SPI-Glass/Assets/Scripts/Laser/ShootLaser.cs
+++ SPI-Glass/Assets/Scripts/Laser/ShootLaser.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ShootLaser : MonoBehaviour
 {
-    
+    public Material material;
+    LaserBeam beam;
+
+    void Update()
+    {
+        Destroy(GameObject.Find("Laser Beam"));
+        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.right, material);
+    }
 }
