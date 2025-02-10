471d9d197c4db3fdc5b7dc153be0ebdbf971fb0a
Sumi Hayasakura <zoomisumi1103@gmail.com>
2025-02-10 12:26:00 -0500

    scene setup

---
+++ SPI-Glass/Assets/Scripts/Dialogue/TriggerBranchDialogue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBranchDialogue : MonoBehaviour
+{
+    public TextAsset inkJSONDialogue1;
+    public TextAsset inkJSONDialogue2;
+    public bool state = false;
+    public int dialogue;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == false)
+        {
+            if (dialogue == 1)
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSONDialogue1, true);
+            }
+            else if (dialogue == 2)
+            {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSONDialogue2, true);
+            }
+            else
+            {
+                Debug.Log("no dialogue available");
+            }
+            state = true;
+        }
+    }
+}
