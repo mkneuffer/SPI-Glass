71db74ceb6d32b36b1dfd469bc690da489debaa4
jtkardon <jtkardon@gmail.com>
2025-02-15 13:26:41 -0500

    RopeSpawningScript Works

--- SPI-Glass/Assets/Scripts/RopeScript.cs
+++ SPI-Glass/Assets/Scripts/RopeScript.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
 
 public class RopeScript : MonoBehaviour
 {
+    [SerializeField] GameObject ropeHead;
+    [SerializeField] GameObject ropeBody;
+    [SerializeField] int length = 10;
+    List<GameObject> ropeParts = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ropeParts.Add(ropeHead);
+        for (int i = 0; i < length; i++)
+        {
+            GameObject instantiatedRope = Instantiate(ropeBody, transform);
+            if (i == 0)
+                instantiatedRope.transform.position = ropeParts[i].transform.position - new Vector3(0, 0.5f, 0);
+            else
+                instantiatedRope.transform.position = ropeParts[i].transform.position - new Vector3(0, 1, 0);
+            HingeJoint joint = instantiatedRope.GetComponent<HingeJoint>();
+            if (joint != null)
+            {
+                joint.connectedBody = ropeParts[i].GetComponent<Rigidbody>();
+                joint.autoConfigureConnectedAnchor = true;
+            }
+            ropeParts.Add(instantiatedRope);
+        }
     }
 }
